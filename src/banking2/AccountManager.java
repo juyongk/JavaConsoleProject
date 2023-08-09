baabf6dc8ba39f64b65da068055c4bdc0c4db2e1
juyongk <kimjuyong1108@gmail.com>
2023-08-09 16:06:39 +0900

    ddd

--- src/banking2/AccountManager.java
+++ src/banking2/AccountManager.java
@@ -2,35 +2,60 @@ package banking2;
 
 import java.util.Scanner;
 
-import banking1.Account;
-
 public class AccountManager {
 	
-	public static Scanner sc = new Scanner(System.in);
+	public Scanner sc = new Scanner(System.in);
 	
-	static Account[] accountArr = new Account[50];
+	Account[] accountArr = new Account[50];
 	
-	static int accAccount = 0;
+	int accAccount = 0;
 
-	public static void makeAccount() {
+	
+	
+	public  void makeAccount() {
 		System.out.println("***신규계좌개설***");
+		System.out.print("보통계좌:");
+		String normalAcc = sc.nextLine();
+		
+		System.out.print("신용계좌:");
+		String highAcc = sc.nextLine();
+		
+		System.out.print("선택:");
+		int choice = sc.nextInt();
+		sc.nextLine();
+		
 		System.out.print("계좌번호:");
 		String accId = sc.nextLine();
+		
 		System.out.print("고객이름:");
 		String cusName = sc.nextLine();
+		
 		System.out.println("잔고:");
 		int balance = sc.nextInt();
+		sc.nextLine();
+		
 		System.out.println("기본이자%(정수형태로입력):");
 		int debt = sc.nextInt();
 		sc.nextLine();
-		accountArr[accAccount++] = new Account(accId,cusName,balance);
-	      System.out.println("계좌계설이 완료되었습니다.");
-	}
-	
-//	양평루피 ㅎㅇ
-	
-	public static void main(String[] args) {
+	    
+	    
+//		Account acct = null;
+		switch(choice) {
+		case 1:
+			accountArr[accAccount++] = new NormalAccount(accId,cusName,balance,debt);
+			 break;
 
-		Scanner scan = new Scanner(System.in);
+		case 2:
+			System.out.print("신용등급(A,B,C등급):");
+			String creditrating = sc.nextLine(); 
+			acct = new HighCreditAccount(accId,cusName,balance,debt,creditrating);
+			break;
+		
+	
+		accountArr[accAccount++] = acct;
+				
+				System.out.println("계좌가 생성되었습니다.");
+			}
 	}
+	
 }
