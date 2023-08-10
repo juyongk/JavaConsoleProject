f2a4c781c543a4a03008d8d61b1e9bf0362d4d4f
juyongk <kimjuyong1108@gmail.com>
2023-08-10 18:05:03 +0900

    2차 프로젝트

--- src/banking3/Account.java
+++ src/banking3/Account.java
@@ -1,4 +1,4 @@
-package banking2;
+package banking3;
 
 public class Account {
 	
---
+++ src/banking3/MenuChoice.java
@@ -0,0 +1,7 @@
+package banking3;
+
+public interface MenuChoice {
+
+	int MAKE=1, deposit=2, withdraw =3, inquire=4, exit=5;
+	
+}
